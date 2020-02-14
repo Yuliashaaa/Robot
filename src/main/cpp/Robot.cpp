7aef9daacdb9f5da499967669fc2ee312e7e2a11
Mirinum <40400823+Mirinum@users.noreply.github.com>
2020-02-14 12:21:40 +0200

    Распаковал архив

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <frc/Joystick.h>
+#include <frc/PWMVictorSPX.h>
+#include <frc/TimedRobot.h>
+#include <frc/Timer.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/livewindow/LiveWindow.h>
+
+#include <frc/GenericHID.h>
+ 
+
+class Robot : public frc::TimedRobot {
+ public:
+  Robot() {
+    m_robotDrive.SetExpiration(0.1);
+    m_timer.Start();
+  }
+
+  void AutonomousInit() override {
+    m_timer.Reset();
+    m_timer.Start();
+  }
+
+  void AutonomousPeriodic() override {
+    // Drive for 2 seconds
+    if (m_timer.Get() < 2.0) {
+      // Drive forwards half speed
+      m_robotDrive.ArcadeDrive(-0.5, 0.0);
+    } else {
+      // Stop robot
+      m_robotDrive.ArcadeDrive(0.0, 0.0);
+    }
+  }
+
+  void TeleopInit() override {}
+
+  void TeleopPeriodic() override {
+    // Drive with arcade style (use right stick)
+    //m_robotDrive.ArcadeDrive(m_stick.GetY()*0.6, m_stick.GetX()*0.6);
+
+    // m_robotDrive.TankDrive(m_stick.GetY(frc::GenericHID::JoystickHand::kLeftHand) * 0.6,
+    //                        m_stick.GetY(frc::GenericHID::JoystickHand::kRightHand) * 0.6);
+
+    m_robotDrive.TankDrive(m_stick.GetRawAxis(5) * 0.6, m_stick.GetRawAxis(1) * 0.6);
+  }
+
+  void TestPeriodic() override {}
+
+ private:
+  // Robot drive system
+  frc::PWMVictorSPX m_left{0};
+  frc::PWMVictorSPX m_right{1};
+  frc::DifferentialDrive m_robotDrive{m_left, m_right};
+
+  frc::Joystick m_stick{0};
+
+
+  
+  frc::LiveWindow& m_lw = *frc::LiveWindow::GetInstance();
+  frc::Timer m_timer;
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
